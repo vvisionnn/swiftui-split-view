15ab596633be2d9fd69e8f2f316f8aa6d998fe45
Ydna <0xEFEFEF@gmail.com>
2024-09-14 02:51:44 +0800

    fix: custom gesture for resolve more conflicts

--- Sources/SplitView/SplitView.swift
+++ Sources/SplitView/SplitView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 public struct SplitView<Sidebar: View, Content: View>: View {
+	private let animation: Animation = .snappy(duration: 0.25, extraBounce: .zero)
 	@State private var offset: Double = .zero
 	@State private var lastDragOffset: Double = .zero
 	@State private var progress: Double = .zero
@@ -24,7 +25,7 @@ public struct SplitView<Sidebar: View, Content: View>: View {
 
 	@MainActor
 	@ViewBuilder
-	var swipeShowBody: some View {
+	var swipeShowContent: some View {
 		GeometryReader { proxy in
 			let maxWidth = proxy.size.width
 			let sidebarWidth = min(sidebarWidth, maxWidth)
@@ -43,23 +44,40 @@ public struct SplitView<Sidebar: View, Content: View>: View {
 				content()
 					.overlay(content: {
 						Rectangle()
-							.opacity(0.3 * progress)
 							.ignoresSafeArea()
+							.onTapGesture {
+								guard progress == 1.0 else { return }
+								withAnimation(animation) {
+									progress = 0
+									offset = 0
+									lastDragOffset = 0
+								}
+							}
+							.opacity(0.3 * progress)
 
 					})
 					.offset(x: offset)
 			}
-			.simultaneousGesture(
-				DragGesture()
-					.onChanged { val in
-						let translation = val.translation.width + lastDragOffset
+		}
+	}
+
+	@MainActor
+	@ViewBuilder
+	var swipeShowBody: some View {
+		if #available(iOS 18.0, *) {
+			swipeShowContent
+				.gesture(SplitViewSwipeGesture(handle: { gesture in
+					let state = gesture.state
+					let translation = gesture.translation(in: gesture.view).x + lastDragOffset
+					let velocity = gesture.velocity(in: gesture.view).x
+					switch state {
+					case .began, .changed:
 						offset = max(0, min(translation, sidebarWidth))
 						progress = max(0, min(offset / sidebarWidth, 1))
-					}
-					.onEnded { val in
+					default:
 						defer { lastDragOffset = offset }
-						withAnimation(.snappy(duration: 0.25, extraBounce: .zero)) {
-							if offset > (sidebarWidth / 2) {
+						withAnimation(animation) {
+							if (velocity + offset) > (sidebarWidth / 2) {
 								offset = sidebarWidth
 								progress = 1
 							} else {
@@ -68,7 +86,37 @@ public struct SplitView<Sidebar: View, Content: View>: View {
 							}
 						}
 					}
-			)
+				}))
+		} else {
+			swipeShowContent
+				.dragGesture(onUpdate: { val in
+					let translation = val.translation.width + lastDragOffset
+					offset = max(0, min(translation, sidebarWidth))
+					progress = max(0, min(offset / sidebarWidth, 1))
+				}, onEnd: { val in
+					let velocity = val.translation.width / 3
+					defer { lastDragOffset = offset }
+					withAnimation(animation) {
+						if (velocity + offset) > (sidebarWidth / 2) {
+							offset = sidebarWidth
+							progress = 1
+						} else {
+							offset = 0
+							progress = 0
+						}
+					}
+				}, onCancel: {
+					defer { lastDragOffset = offset }
+					withAnimation(animation) {
+						if offset > (sidebarWidth / 2) {
+							offset = sidebarWidth
+							progress = 1
+						} else {
+							offset = 0
+							progress = 0
+						}
+					}
+				})
 		}
 	}
 
@@ -105,6 +153,84 @@ extension SplitView {
 	}
 }
 
+extension SplitView {
+	struct SplitViewSwipeGesture: UIGestureRecognizerRepresentable {
+		var handle: (UIPanGestureRecognizer) -> Void
+		func makeUIGestureRecognizer(context: Context) -> UIPanGestureRecognizer {
+			UIPanGestureRecognizer()
+		}
+
+		func updateUIGestureRecognizer(_ recognizer: UIPanGestureRecognizer, context: Context) {}
+		func handleUIGestureRecognizerAction(_ recognizer: UIPanGestureRecognizer, context: Context) {
+			handle(recognizer)
+		}
+	}
+}
+
+struct DragGestureViewModifier: ViewModifier {
+	@GestureState private var isDragging: Bool = false
+	@State var gestureState: GestureStatus = .idle
+
+	var onStart: (() -> Void)?
+	var onUpdate: ((DragGesture.Value) -> Void)?
+	var onEnd: ((DragGesture.Value) -> Void)?
+	var onCancel: (() -> Void)?
+
+	func body(content: Content) -> some View {
+		content
+			.simultaneousGesture(
+				DragGesture()
+					.updating($isDragging) { _, isDragging, _ in
+						isDragging = true
+					}
+					.onChanged(onDragChange(_:))
+					.onEnded(onDragEnded(_:))
+			)
+			.onChange(of: gestureState) { state in
+				guard state == .started else { return }
+				gestureState = .active
+			}
+			.onChange(of: isDragging) { value in
+				if value, gestureState != .started {
+					gestureState = .started
+					onStart?()
+				} else if !value, gestureState != .ended {
+					gestureState = .cancelled
+					onCancel?()
+				}
+			}
+	}
+
+	func onDragChange(_ value: DragGesture.Value) {
+		guard gestureState == .started || gestureState == .active else { return }
+		onUpdate?(value)
+	}
+
+	func onDragEnded(_ value: DragGesture.Value) {
+		gestureState = .ended
+		onEnd?(value)
+	}
+
+	enum GestureStatus: Equatable {
+		case idle
+		case started
+		case active
+		case ended
+		case cancelled
+	}
+}
+
+extension View {
+	func dragGesture(
+		onStart: (() -> Void)? = nil,
+		onUpdate: ((DragGesture.Value) -> Void)? = nil,
+		onEnd: ((DragGesture.Value) -> Void)? = nil,
+		onCancel: (() -> Void)? = nil
+	) -> some View {
+		modifier(DragGestureViewModifier(onStart: onStart, onUpdate: onUpdate, onEnd: onEnd, onCancel: onCancel))
+	}
+}
+
 #Preview("Always Show Sidebar", body: {
 	SplitView(style: .alwaysShow) {
 		Rectangle()
@@ -114,6 +240,9 @@ extension SplitView {
 		Rectangle()
 			.foregroundStyle(Color(uiColor: .systemMint))
 			.ignoresSafeArea()
+			.overlay {
+				Text("Content")
+			}
 	}
 })
 
@@ -122,9 +251,7 @@ extension SplitView {
 		ScrollView(.vertical) {
 			LazyVStack {
 				ForEach(0 ..< 100) { index in
-					Button(action: {
-						debugPrint("item index: \(index)")
-					}) {
+					Button(action: {}) {
 						Text("Item \(index)")
 							.foregroundStyle(Color(uiColor: .label))
 							.frame(maxWidth: .infinity)
@@ -138,5 +265,8 @@ extension SplitView {
 		Rectangle()
 			.foregroundStyle(Color(uiColor: .systemMint))
 			.ignoresSafeArea()
+			.overlay {
+				Text("Content")
+			}
 	}
 })
